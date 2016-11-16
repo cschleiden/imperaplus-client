ecc4080d63e4a2438f25f51380774ed0e1493492
cschleiden <cschleiden@live.de>
2016-11-15 23:22:24 -0800

    Add client factory

---
+++ src/clients/clientFactory.ts
@@ -0,0 +1,18 @@
+import { AccountClient } from "external/imperaClients";
+
+// TODO: CS: Retrieve from config
+const baseUri = "http://localhost:57676/";
+
+const clientCache: [any, any][] = [];
+
+export function getClient<TClient>(clientType: new (baseUri: string) => TClient): TClient {
+    for (let [cacheClientType, cachedInstance] of clientCache) {
+        if (cacheClientType === clientType) {
+            return cachedInstance;
+        }
+    }
+
+    let instance = new clientType(baseUri);
+    clientCache.push([clientType, instance]);
+    return instance;
+}
