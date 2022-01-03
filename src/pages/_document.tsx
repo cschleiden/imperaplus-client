4f9465093f0f602096adb5bb6df6cf63f0243586
Christopher Schleiden <cschleiden@live.de>
2022-01-03 09:26:27 -0800

    Add new relic

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+    render() {
+        return (
+            <Html>
+                <Head>
+                    <script
+                        async
+                        type="text/javascript"
+                        src="/js/newrelic.js"
+                    />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
+}
+
+export default MyDocument;
