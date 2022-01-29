6ea107651a80c96bbf53d588ab33bb265e92dd8d
Christopher Schleiden <cschleiden@live.de>
2022-01-28 17:22:49 -0800

    Parse to boolean

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,3 +1,4 @@
 export const baseUri = process.env.NEXT_PUBLIC_BASE_URI;
 export const imageBaseUri = process.env.NEXT_PUBLIC_IMAGE_BASE_URI;
-export const useSecureCookies = process.env.NEXT_PUBLIC_USE_SECURE_COOKIES;
+export const useSecureCookies: boolean =
+    process.env.NEXT_PUBLIC_USE_SECURE_COOKIES === "true";
