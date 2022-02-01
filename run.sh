3f5433a041b29b582d85ffc0eaa5d27ba08f096f
Christopher Schleiden <cschleiden@live.de>
2022-01-31 19:52:24 -0800

    Improve prod container startup time

--- run.sh
+++ run.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-# Ugly work-around for broken Next.js public runtime config
-grep -Irl "#{BaseUri}#" .next/ | xargs sed -i "s~#{BaseUri}#~$BASE_URI~g"
+if [ -n "$BASE_URI" ]; then
+  # Ugly work-around for broken Next.js public runtime config
+  grep -Irl "#{BaseUri}#" .next/ | xargs sed -i "s~#{BaseUri}#~$BASE_URI~g"
+fi
+
 node server.js
