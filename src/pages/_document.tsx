d7ddaa8a645cec7fce9bdd107f9daa09efa3ecef
Christopher Schleiden <cschleiden@live.de>
2023-02-05 10:34:28 -0800

    Fix

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,16 +1,9 @@
-import Document, { Head, Html, Main, NextScript } from "next/document";
+import Document, { Html, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head>
-                    <script
-                        async
-                        type="text/javascript"
-                        src="/js/newrelic.js"
-                    />
-                </Head>
                 <body>
                     <Main />
                     <NextScript />
